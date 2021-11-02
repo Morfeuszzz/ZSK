17ac16fe53f20fa6a11df9ebe7c2c00664f61b28
Sebastian <sebastian.malicki02@gmail.com>
2021-11-02 19:06:12 +0100

    02.11.2021

---
+++ 02.11.2021/tabela_delete.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>uzytkownicy</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <h4>Użytkownicy</h4>
+      <?php
+        require_once './scripts/connect.php';
+        $sql = "SELECT * FROM `users`";
+        $result = $connect->query($sql);
+        // print_r($result);
+        echo<<<TABLE
+        <table >
+         <tr>
+             <th>id</th>
+             <th>imie</th>
+             <th>Nazwisko</th>
+             <th>Data urodzenia</th>
+         </tr>
+TABLE;
+        while ($rows = $result->fetch_assoc()) {
+          echo<<<ROW
+          <tr>
+          <td>$rows[id]</td>
+          <td>$rows[name]</td>
+          <td>$rows[surname]</td>
+          <td>$rows[birthday]</td>
+          <td><a href="./scripts/delete.php?id=$rows[id]">Usuń</a></td>
+          </tr>
+ROW;
+        }
+echo "</table>";
+$connect->close();
+       ?>
+  </body>
+</html>
